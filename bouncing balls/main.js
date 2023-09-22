08a9b0382f911d78f4e3d7656e8a57618dfa493b
Jedla16 <131918731+Jedla16@users.noreply.github.com>
2023-09-22 20:53:40 +0200

    Add files via upload

---
+++ bouncing balls/main.js
@@ -0,0 +1,96 @@
+// setup canvas
+
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
+
+// function to generate random number
+
+function random(min, max) {
+  const num = Math.floor(Math.random() * (max - min + 1)) + min;
+  return num;
+}
+
+// function to generate random color
+
+function randomRGB() {
+  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+}
+// class for ball objects
+  class Ball{
+    constructor(x,y,velX,velY, color, size){
+      this.x = x;
+      this.y = y;
+      this.velX = velX;
+      this.velY = velY;
+      this.color = color;
+      this.size = size;
+    }
+  
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+
+
+/*
+const testBall = new Ball(1000,200,40,40,"red",100);
+testBall.draw();
+*/
+
+update() {
+  if ((this.x + this.size) >= width){
+    this.velX = -this.velX;
+  }
+
+  if ((this.x + this.size) <= 0){
+    this.velX = -this.velX;
+  }
+
+  if ((this.y + this.size) >= height){
+    this.velY = -this.velY;
+  }
+  
+  if ((this.y + this.size) <= 0){
+    this.velY = -this.velY;
+  }
+
+  this.x += this.velX;
+  this.y += this.velY;
+}
+}
+
+// preparing the balls
+const balls = [];
+
+  while(balls.length < 20){
+  const size = random(10,20);
+  const ball = new Ball(
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-6,6),
+    random(-6,6),
+    randomRGB(),
+    size,
+  );
+  
+  balls.push(ball);
+
+}
+// generating the balls
+function loop(){
+  ctx.fillStyle = "rgba(0,0,0,0.25)";
+  ctx.fillRect(0,0,width, length);
+  for (const ball of balls){
+    ball.draw();
+    ball.update();
+
+  }
+  requestAnimationFrame(loop);
+}
+// call it
+loop();
